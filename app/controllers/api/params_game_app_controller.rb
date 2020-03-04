3260618a2e094f8bd78a265501de5b0d54015b65
Scott Geryol <sgeryol@gmail.com>
2020-03-03 21:28:01 -0600

    created params

--- app/controllers/api/params_game_app_controller.rb
+++ app/controllers/api/params_game_app_controller.rb
@@ -1,7 +1,21 @@
 class Api::ParamsGameAppController < ApplicationController
   def params_action
     input_message = params["my_name"]
-    @output_message = "yo #{input_message}."
+    @output_message = input_message.upcase
+    if @output_message.starts_with?("A")
+      @output_message += " (hey, your name starts with A)."
+    end
+
     render "params.json.jb"
+  end
+
+  def guess_action
+    winning_number = 32
+    input_guess = params["my_guess"].to_i
+    if input_guess > winning_number
+      @message = "yO"
+    end
+
+    render "guess.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/params" => "params_game_app#params_action"
+    get "/guess" => "params_game_app#guess_action"
   end
 end
